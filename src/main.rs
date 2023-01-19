1f0144b60e62877b4a955126010178851d2e9a11
prex-aliya <droved-through@proton.me>
2023-01-19 17:18:43 +0000

    added create dir

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,34 @@
-/* Variables {{{ */
-const WRITEDIR: &str    = "/tmp/foo";
-/* }}}*/
+use std::fs;
 
+#[cfg(test)]
+mod test;
 
+const WRITEDIR: &str = "/tmp/foo";
+const VERBOSE: bool = true;
 
 /* misc {{{ */
-is_verbose(input: &str, verbose: bool) {
+fn is_verbose(input: &str, verbose: bool) {
     if verbose {
         println!("[*] {}", input);
     }
 }
+fn create_dir(input: &str) -> Result<String, std::io::Error> {
+    let s = String::new();
+    fs::create_dir_all(input).expect("create_dir failed to make file");
+
+    Ok(s)
+}
 /* }}} */
-
-
 
 /* vvv */
 
-
-
 fn shell(verbose: bool) {
     is_verbose("", verbose);
+
+
 }
 
 fn main() {
-    let verbose = true;
-    shell(verbose);
+    _=create_dir(WRITEDIR);
+    shell(VERBOSE);
 }
