cdab6f04abbf3cc41a32d8d379c24a5714367eb7
prex-aliya <droved-through@proton.me>
2023-01-21 01:05:38 +0000

    Backup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::fs;
-
 #[cfg(test)]
 mod test;
 
@@ -14,21 +12,45 @@ fn is_verbose(input: &str, verbose: bool) {
 }
 fn create_dir(input: &str) -> Result<String, std::io::Error> {
     let s = String::new();
-    fs::create_dir_all(input).expect("create_dir failed to make file");
+    std::fs::create_dir_all(input).expect("create_dir failed to make file");
 
     Ok(s)
 }
+fn get_input() -> String {
+    let mut input_text: String = String::new();
+    std::io::stdin()
+        .read_line(&mut input_text)
+        .expect("failed to read from stdin");
+
+    input_text
+}
 /* }}} */
+fn shellopen(input: Vec<Vec<String>>) {}
+
+
+
+
 
 /* vvv */
 
 fn shell(verbose: bool) {
-    is_verbose("", verbose);
+    is_verbose("Starting In Shell Mode", verbose);
+    _=create_dir(WRITEDIR);
 
+    let list: Vec<Vec<String>> = Default::default();
 
+    'main: loop {
+        let input = get_input();
+
+        match input.split_whitespace().next() {
+            Some("o") | Some("open") => { shellopen(list); },
+            _ => {},
+        }
+
+        break 'main;
+    }
 }
 
 fn main() {
-    _=create_dir(WRITEDIR);
     shell(VERBOSE);
 }
--- src/test.rs
+++ src/test.rs
@@ -5,5 +5,51 @@ use super::*;
 
 #[test]
 fn test() {
-    open_file();
+    //open_file();
+    testing();
+}
+
+use ncurses::*;
+const REGULAR_PAIR: i16 = 0;
+const HIGHLIGHT_PAIR: i16 = 1;
+
+fn testing() {
+    initscr();
+
+    start_color();
+    init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK);
+    init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
+
+    let mut quit = false;
+    let mut todos = vec![
+        "Bey a bread",
+        "Write a todo app",
+        "Make a cup of tea"
+    ];
+    let todo_curr: usize = 1;
+
+    while !quit {
+        for (row, todo) in todos.iter().enumerate() {
+            let pair = {
+                if todo_curr == row {
+                    REGULAR_PAIR
+                } else {
+                    HIGHLIGHT_PAIR
+                }
+            };
+
+            attron(COLOR_PAIR(pair));
+            mv(row as i32, 0);
+            attroff(COLOR_PAIR(pair));
+            addstr(*todo);
+        }
+
+        let key = getch();
+        match key as u8 as char {
+            'q' => quit = true,
+            _ => {}
+        }
+    }
+
+    endwin();
 }
