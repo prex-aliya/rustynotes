7a4c7b8341864032ca7890363fa8c8de415e98e8
prex-aliya <droved-through@proton.me>
2023-01-21 03:13:09 +0000

    base application

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,93 @@
-#[cfg(test)]
-mod test;
+use std::cmp::min;
 
-const WRITEDIR: &str = "/tmp/foo";
-const VERBOSE: bool = true;
+use ncurses::*;
+const REGULAR_PAIR: i16 = 0;
+const HIGHLIGHT_PAIR: i16 = 1;
 
-/* misc {{{ */
-fn is_verbose(input: &str, verbose: bool) {
-    if verbose {
-        println!("[*] {}", input);
+type Id = usize;
+
+#[derive(Default)]
+struct Ui {
+    lst_curr: Option<Id>,
+}
+
+impl Ui {
+    fn list_element(&mut self, input: &mut Vec<&str>, curr: usize) {
+        for (row, todo) in input.iter().enumerate() {
+            let pair = {
+                if curr == row {
+                    HIGHLIGHT_PAIR
+                } else {
+                    REGULAR_PAIR
+                }
+            };
+
+            attron(COLOR_PAIR(pair));
+            mv(row as i32, 0);
+            addstr(*todo);
+            attroff(COLOR_PAIR(pair));
+        }
     }
-}
-fn create_dir(input: &str) -> Result<String, std::io::Error> {
-    let s = String::new();
-    std::fs::create_dir_all(input).expect("create_dir failed to make file");
+    fn add_elements(&mut self, input: &mut Vec<&str>) {
+        nocbreak();
+        echo();
 
-    Ok(s)
-}
-fn get_input() -> String {
-    let mut input_text: String = String::new();
-    std::io::stdin()
-        .read_line(&mut input_text)
-        .expect("failed to read from stdin");
+        let mut output: String = Default::default();
 
-    input_text
-}
-/* }}} */
-fn shellopen(input: Vec<Vec<String>>) {}
-
-
-
-
-
-/* vvv */
-
-fn shell(verbose: bool) {
-    is_verbose("Starting In Shell Mode", verbose);
-    _=create_dir(WRITEDIR);
-
-    let list: Vec<Vec<String>> = Default::default();
-
-    'main: loop {
-        let input = get_input();
-
-        match input.split_whitespace().next() {
-            Some("o") | Some("open") => { shellopen(list); },
-            _ => {},
+        let mut ch = getch();
+        while ch as u8 as char != '\n' {
+            output.push(ch as u8 as char);
+            ch = getch();
         }
 
-        break 'main;
+        noecho();
+        cbreak();
+
     }
 }
 
 fn main() {
-    shell(VERBOSE);
+    initscr();
+    noecho(); /* doesnt echo what you type */
+    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+
+    start_color();
+    init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK);
+    init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
+
+    let mut quit = false;
+    let mut todos = vec![
+        "Bey a bread",
+        "Write a todo app",
+        "Make a cup of tea"
+    ];
+    let mut dones = Vec::<&str>::new();
+    let mut done_curr: usize = 0;
+    let mut todo_curr: usize = 0;
+
+    let mut ui = Ui::default();
+    while !quit {
+
+        ui.list_element(&mut todos, todo_curr);
+        //ui.label("------------------------------");
+        ui.list_element(&mut dones, done_curr);
+
+
+        refresh();
+
+        let key = getch();
+        match key as u8 as char {
+            'q' => quit = true,
+            'w' | 'k' => if todo_curr > 0 { todo_curr -= 1; },
+            's' | 'j' => todo_curr = min(todo_curr + 1, todos.len() - 1), /* ? */
+            'a' => ui.add_elements(&mut todos),
+            _ => {}
+        }
+    }
+
+    endwin();
 }
+
+#[cfg(test)]
+mod test;
+
